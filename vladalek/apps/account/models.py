de5ac1934b1fde3e019f3cff873f4ac786d3b91c
metimol <rdgo16480@gmail.com>
2022-01-22 02:01:05 +0200

    улучшил аккаунты

--- vladalek/apps/account/models.py
+++ vladalek/apps/account/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class Profile(AbstractUser):
 	code = models.TextField(blank=True, null=True, verbose_name='Код')
-	avatar = models.CharField(blank=True, null=True, max_length=8, verbose_name="Аватар")
+	avatar = models.URLField(blank=True, null=True, verbose_name="Аватар")
 	email = models.EmailField(verbose_name='Почта', unique=True)
 	github = models.URLField(verbose_name='Ссылка на GitHub', blank=True, null=True)
 	instagram = models.URLField(verbose_name='Ссылка на Instagram', blank=True, null=True)
--- vladalek/static/js/account/about.js
+++ vladalek/static/js/account/about.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const image = document.querySelector('.image')
+	const github = document.getElementById("github")
 	image.addEventListener('click', () => {
 		document.querySelector('.edit_avatar').style.display = "block";
+	})
+	github.addEventListener('click', () => {
+		document.querySelector('.github').style.display = "block";
 	})
 })
