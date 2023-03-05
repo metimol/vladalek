556ada817475bdbe82c8d31e5e75c6619ae50b36
Metimol <rdgo16480@gmail.com>
2023-03-05 02:25:46 +0100

    First version

--- vladalek/apps/blog/models.py
+++ vladalek/apps/blog/models.py
@@ -3,7 +3,8 @@ from tinymce.models import HTMLField
 from account.models import Profile
 
 class Categories(models.Model):
-	category = models.CharField(max_length=25,  verbose_name='Категорія', help_text='Назва категорії')
+	category = models.CharField(max_length=100,  verbose_name='Категорія')
+	icon = models.CharField(max_length=100, verbose_name="Іконка")
 	def __str__ (self):
 		return self.category
 	class Meta:
@@ -13,9 +14,9 @@ class Categories(models.Model):
 class Articles(models.Model):
 	categories = models.ForeignKey(Categories, null=True, related_name = "articles", on_delete = models.SET_NULL, verbose_name='Категорія')
 	article_author = models.ForeignKey(Profile, related_name = "articles", on_delete = models.CASCADE, verbose_name='Автор статті')
-	article_title = models.CharField(max_length=20, help_text='Введіть назву статті', verbose_name='Назва статті')
-	article_text = HTMLField(help_text='Текст статті', verbose_name='Текст статті')
-	article_about = models.TextField(help_text='Про що стаття', verbose_name='Опис статті', null=True)
+	article_title = models.CharField(max_length=50, verbose_name='Назва статті')
+	article_text = HTMLField(verbose_name='Текст статті')
+	article_about = models.CharField(max_length=100, verbose_name='Опис статті')
 	pub_date = models.DateField(auto_now_add=True)
 	fixed = models.BooleanField(default=False, verbose_name="Закріпити")
 	favorites = models.ManyToManyField(Profile, related_name="favourites", blank=True, verbose_name = "Обране")
--- vladalek/apps/blog/views.py
+++ vladalek/apps/blog/views.py
@@ -1,4 +1,3 @@
-import string
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -8,39 +7,17 @@ from .forms import ComentForm
 from account.models import Profile
 
 def index(request):
-	category = request.GET.get('category', '')
-	page_num = request.GET.get('page', '')
-	categories_list = Categories.objects.order_by('category')
-	if category:
+	categories = request.GET.get('category', '')
+	categories_list = Categories.objects.all()
+	if categories:
 		try:
-			cat = Categories.objects.get(category=category)
+			cat = Categories.objects.get(category=categories)
 			articles_list = cat.articles.order_by('-id')
+			return render(request, "blog/index.html", {"articles_list": articles_list})
 		except:
 			return render(request, "404.html")
 	else:
-		articles_list = Articles.objects.order_by('-id')
-	if articles_list:
-		paginator = Paginator(articles_list, 10)
-		if not page_num:
-			page_num = 1
-		page = paginator.get_page(page_num)
-		page_list = []
-		if paginator.num_pages>1:
-			if paginator.num_pages>7:
-				if int(page_num)<paginator.num_pages-5:
-					for i in range(int(page_num), int(page_num)+5):
-						page_list.append(i)
-				else:
-					for i in range(paginator.num_pages-5, paginator.num_pages):
-						page_list.append(i)
-			else:
-				for i in range(1, paginator.num_pages+1):
-					page_list.append(i)
-		context = {'page': page, 'articles_list': page.object_list, 'categories_list': categories_list, 'category': category, 'paginator': paginator, 'page_list':page_list}
-	else:
-		return render(request, "blog/index.html", {"categories_list": categories_list})
-	
-	return render(request, 'blog/index.html', context,)
+		return render(request, "blog/categories.html", {"categories": categories_list})
 
 def detail(request, article_id):
 	try:
