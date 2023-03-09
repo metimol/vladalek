42616505f4233ca7447a96f8a1b5c97b17d0bd41
Metimol <rdgo16480@gmail.com>
2023-03-10 00:53:28 +0100

    Add analytics tools

--- vladalek/apps/blog/models.py
+++ vladalek/apps/blog/models.py
@@ -14,9 +14,9 @@ class Categories(models.Model):
 class Articles(models.Model):
 	categories = models.ForeignKey(Categories, null=True, related_name = "articles", on_delete = models.SET_NULL, verbose_name='Категорія')
 	article_author = models.ForeignKey(Profile, related_name = "articles", on_delete = models.CASCADE, verbose_name='Автор статті')
-	article_title = models.CharField(max_length=50, verbose_name='Назва статті')
+	article_title = models.CharField(max_length=500, verbose_name='Назва статті')
 	article_text = HTMLField(verbose_name='Текст статті')
-	article_about = models.CharField(max_length=100, verbose_name='Опис статті')
+	article_about = models.CharField(max_length=500, verbose_name='Опис статті')
 	pub_date = models.DateField(auto_now_add=True)
 	favorites = models.ManyToManyField(Profile, related_name="favourites", blank=True, verbose_name = "Обране")
 	def __str__(self):
@@ -28,7 +28,7 @@ class Articles(models.Model):
 class Coments(models.Model):
 	articles = models.ForeignKey(Articles, on_delete=models.CASCADE, related_name='coments', verbose_name='До якої статті?')
 	coment_author = models.ForeignKey(Profile, related_name = "coments", on_delete = models.CASCADE, verbose_name='Автор коментаря')
-	coment_text = models.CharField(max_length=200, verbose_name='Текст коментаря')
+	coment_text = models.CharField(max_length=10000, verbose_name='Текст коментаря')
 	pub_date = models.DateTimeField(auto_now_add=True)
 	def __str__(self):
 		return self.coment_text
