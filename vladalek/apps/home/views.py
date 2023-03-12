fb28c5fef6c733ee92702559dcf8507c35540da4
Metimol <rdgo16480@gmail.com>
2023-03-12 01:51:28 +0100

    Version 2.1.0

--- vladalek/apps/home/views.py
+++ vladalek/apps/home/views.py
@@ -3,14 +3,7 @@ from blog.models import Articles
 from account.models import Profile
 
 def index(request):
-	fixed_list = Articles.objects.filter(fixed=True)
-	articles_list = Articles.objects.order_by('-id')[:3]
-	if Profile.objects.filter(username="metimol").exists():
-		admin = Profile.objects.get(username="metimol")
-	else:
-		admin = None
-	context = {'fixed_list': fixed_list, 'articles_list': articles_list, "admin": admin}
-	return render(request, 'home/index.html', context,)
+	return render(request, 'home/index.html',)
 
 def sitemap(request):
 	
